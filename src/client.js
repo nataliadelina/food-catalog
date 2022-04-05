8f784dd35a63be9a74ec371e4c3d7632be5128bc
Natalia <null>
2022-04-05 11:52:32 +0200

    client sanity

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+    projectId: "1ihc5ytj",
+    dataset: "production", // this is from those question during 'sanity init'
+});
