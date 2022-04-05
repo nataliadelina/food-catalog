7fb09d4d2efe655c6326ee41ef97140230fe6f09
Natalia <null>
2022-04-05 13:50:48 +0200

    fix error with activeclassname

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav>
-            <NavLink to="/" className="link" activeClassName="active-link">😋Yummy Food's</NavLink>
-            <NavLink to="/about" className="link" activeClassName="active-link">about</NavLink>
-            <NavLink to="/foods" className="link" activeClassName="active-link">food</NavLink>
-            <NavLink to="/filteredFoods/:slug" className="link" activeClassName="active-link">filteredFoods</NavLink>
+            <NavLink to="/" activeclassname="active-link">😋Yummy Food's</NavLink>
+            <NavLink to="/about" activeclassname="active-link">about</NavLink>
+            <NavLink to="/foods" activeclassname="active-link">food</NavLink>
+            <NavLink to="/filteredFoods/:slug" activeclassname="active-link">filteredFoods</NavLink>
         </nav>
     )
 }
